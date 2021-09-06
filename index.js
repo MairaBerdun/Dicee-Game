8570c4624fca85954adc3a76015fb14ae7702c58
Maira Berdun <mberdun.95@gmail.com>
2021-09-06 18:48:01 -0300

    Fixed roots

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1; //doy un numero aleatorio
 
 let randomImage1 = "dice" + randomNumber1 + ".png"; // doy una imagen aleatoria de 1 a 6
 
-let randomImageSource = "images/" + randomImage1; //tendremos /images.dice1.png a dice6.png
+let randomImageSource = "./images/" + randomImage1; //tendremos /images.dice1.png a dice6.png
 
 document.querySelectorAll("img")[0].setAttribute("src", randomImageSource); //tomo el elemento y cambio el src 
 
@@ -15,7 +15,7 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1; //genero un num aleatorio
 
 let randomImage2 = "dice" + randomNumber2 + ".png"; //genero una foto aleatoria en el rango y lo concatenamos 
 
-let randomImageSource2 = "images/" + randomImage2; //asocio la foto aleatoria a la carpeta images
+let randomImageSource2 = "./images/" + randomImage2; //asocio la foto aleatoria a la carpeta images
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); 
 
